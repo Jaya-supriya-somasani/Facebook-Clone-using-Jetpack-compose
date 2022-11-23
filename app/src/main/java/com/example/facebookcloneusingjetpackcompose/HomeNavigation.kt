7b642fd4fb6abe3f0ccdab88079f5b59eb13154c
jayasupriyasomasani <somasanijayasupriya@gmail.com>
2022-11-23 17:10:15 +0530

    Home page navigation graph

---
+++ app/src/main/java/com/example/facebookcloneusingjetpackcompose/HomeNavigation.kt
@@ -0,0 +1,37 @@
+package com.example.facebookcloneusingjetpackcompose
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun HomeNavigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = Screen.HomeScreen.route) {
+        composable(route=Screen.HomeScreen.route){
+            Home()
+        }
+        composable(route=Screen.ProfileScreen.route){
+            ProfilePage()
+        }
+        composable(route=Screen.VideosScreen.route){
+            VideosPage()
+        }
+        composable(route=Screen.FriendReqScreen.route){
+            FriendsRequestsPage()
+        }
+        composable(route=Screen.NotificationsScreen.route){
+            NotificationsPage()
+        }
+        composable(route=Screen.MoreSettingsScreen.route){
+            MoreSettings()
+        }
+        composable(route=Screen.SingUpScreen.route){
+            SignupPage()
+        }
+        composable(route=Screen.ForgotPasswordScreen.route){
+            ForgotPasswordPage()
+        }
+    }
+}
